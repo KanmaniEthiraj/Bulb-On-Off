78ab8cc185a92066f3779b24250f6a2f230d3481
KanmaniEthiraj <kanmaniethiraj@gmail.com>
2025-03-19 21:13:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+/*declare variable in three types var,let,const*/
+ var on_btn= document.getElementById("on");
+ var off_btn= document.getElementById("off");
+ var img= document.getElementById("bulb");
+
+ function bulb_on(){
+    img.src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKRJzpMqn8Rw_tPcMVFb4tRV3OKqJBSFvD5g&s"
+ }
+
+ function bulb_off(){
+    img.src= "https://www.shutterstock.com/image-illustration/3d-rendering-clear-light-bulb-260nw-699554389.jpg"
+ }
